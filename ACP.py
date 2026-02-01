a795eed5916ae6f7462b9ce50fb88cc39f5b7749
anantya12 <verma.anantya12@gmail.com>
2026-02-01 17:24:24 +0530

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+a = "12th March"
+s = "12th April"
+r = "31st December"
+n = "8th August"
+d= "18th May"
+
+print("Anantya's birthday is", a)
+print("Samar's birthday is", s)
+print("Riddhi's birthday is", r)
+print("Nitin's birthday is", n)
+print("Anadya's birthday is", d)
